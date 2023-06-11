a04471b225fa282e29db75af8ee8109aea5ad2f3
cheraa-git <chernetsov.aa@mail.ru>
2023-06-11 23:51:00 +0400

    m-2 l-19 redux-2: lesson (createActon + createReducer)

--- src/store/task.js
+++ src/store/task.js
@@ -1,36 +1,46 @@
-const TASK_UPDATED = 'task/updated'
-const TASK_DELETED = 'task/deleted'
+import { createAction, createReducer } from '@reduxjs/toolkit'
+
+const initialState = [
+  { id: 1, title: 'Task 1', completed: false },
+  { id: 2, title: 'Task 2', completed: false }
+]
+const update = createAction('task/updated')
+const remove = createAction('task/removed')
+
 
 export const taskCompleted = (id) => {
-  return {
-    type: TASK_UPDATED,
-    payload: { id, completed: true }
-  }
+  return update({ id, completed: true })
 }
 
 export const titleChanged = (id) => {
-  return {
-    type: TASK_UPDATED,
-    payload: { id, title: `New Title for ${id}` }
-  }
+  return update({ id, title: `New Title for ${id}` })
 }
 
 export const taskDeleted = (id) => {
-  return {
-    type: TASK_DELETED,
-    payload: { id }
-  }
+  return remove({ id })
 }
 
-function taskReducer(state = [], action) {
+
+const taskReducer = createReducer(initialState, builder => {
+  builder
+    .addCase(update, (state, action) => {
+      const elementIndex = state.findIndex(el => el.id === action.payload.id)
+      state[elementIndex] = { ...state[elementIndex], ...action.payload }
+    })
+    .addCase(remove, (state, action) => {
+      return state.filter(el => el.id !== action.payload.id)
+    })
+})
+
+function _taskReducer(state = [], action) {
   let elementIndex, newArray
   switch (action.type) {
-    case TASK_UPDATED:
+    case update.type:
       newArray = [...state]
       elementIndex = newArray.findIndex(el => el.id === action.payload.id)
       newArray[elementIndex] = { ...newArray[elementIndex], ...action.payload }
       return newArray
-    case TASK_DELETED:
+    case remove.type:
       newArray = [...state]
       elementIndex = newArray.findIndex(el => el.id === action.payload.id)
       newArray = newArray.filter(el => el.id !== action.payload.id)
