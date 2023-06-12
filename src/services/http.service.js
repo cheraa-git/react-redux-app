43288c1204a1665f7e3d552b00a343f17c4164b5
cheraa-git <chernetsov.aa@mail.ru>
2023-06-12 09:56:08 +0400

    m-2 l-19 redux-2: lesson (configureStore)

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-  baseURL: 'https://js1onplaceholder.typicode.com/'
+  baseURL: 'https://jsonplaceholder.typicode.com/'
 })
 
 const httpService = {
--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,15 @@
-import { applyMiddleware, compose, createStore } from 'redux'
 import taskReducer from './task'
 import { logger } from './middleware/logger'
-import { thunk } from './middleware/thunk'
+import { configureStore } from '@reduxjs/toolkit'
 
-const middleWareEnhancer = applyMiddleware(logger, thunk)
 
-export function configureStore() {
-  return createStore(
-    taskReducer,
-    compose(middleWareEnhancer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+export function createStore() {
+  return configureStore({
+      reducer: taskReducer,
+      middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+      devTools: process.env.NODE !== 'production'
+    },
   )
 }
 
-export default configureStore
+export default createStore
